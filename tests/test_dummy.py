2d0ba4735ef917b45e325087c3c32d9974a2ab9b
Ismael Marin <ismitae.imm@gmail.com>
2023-09-19 19:19:14 +0100

    Added coverage

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,13 @@
-def test_dummy():
-    assert 5 == 5
+from clustlib.dummy import fib
+
+
+def test_dummy_0():
+    assert fib(0) == 0
+
+
+def test_dummy_1():
+    assert fib(1) == 1
+
+
+def test_fib_10():
+    assert fib(10) == 55
