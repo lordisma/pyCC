9b545fc135c6f759d6ff7b4c4c9bb5d9e198fd83
Ismael Marin <ismitae.imm@gmail.com>
2025-08-11 17:06:51 +0200

    Basic test for genetic algorithm

--- clustlib/gac/brkga.py
+++ clustlib/gac/brkga.py
@@ -1,11 +1,13 @@
 import numpy as np
-import copy as cp
 
 from typing import Sequence
-from ..utils.matrix import ConstraintMatrix
 
+import math
 from clustlib.gac.base import GeneticClustering
 
+import logging
+
+logger = logging.getLogger(__name__)
 
 class BRKGA(GeneticClustering):
     """
@@ -20,48 +22,61 @@ class BRKGA(GeneticClustering):
         init (str): Método de inicialización de centroides.
         max_iter (int): Máximo número de iteraciones.
         tol (float): Tolerancia para criterio de convergencia.
-        custom_initial_centroids (array-like): Centroides definidos por el usuario (opcional).
         constraints (Sequence[Sequence]): Restricciones must-link y cannot-link.
         population_size (int): Tamaño de la población genética.
         percentage_elite (float): Porcentaje de individuos considerados elite.
         probability_mutation (float): Porcentaje de mutantes en cada generación.
         pbt_inherit (float): Probabilidad de herencia en el operador de cruce.
-        mu (float): Parámetro de mutación.
     """
     def __init__(
         self,
+        constraints: Sequence[Sequence],
         n_clusters=8,
         init="random",
         max_iter=300,
         tol=1e-4,
-        custom_initial_centroids=None,
-        constraints: Sequence[Sequence] = None,
         population_size=20,
         percentage_elite=0.3,
         probability_mutation=0.2,
-        pbt_inherit=None,
-        mu=None,
+        pbt_inherit=0.1
     ):
-        self._delta_centroid = None
         self.n_clusters = n_clusters
-        self._centroids_distance = np.zeros((self.n_clusters, self.n_clusters))
         self.init = init
         self.max_iter = max_iter
         self.tol = tol
-        self.custom_initial_centroids = custom_initial_centroids
         self.centroids = None
-        self.constraints = ConstraintMatrix(constraints)
+        self.constraints = constraints
         self._population_size = population_size
-        self.solution_archive = None
+        self._dim = constraints.shape[0]
 
-        self._num_elite = int(self._population_size * percentage_elite)
-        self._num_mutants = int(self._population_size * probability_mutation)
+        self._num_elite = math.ceil(self._population_size * percentage_elite)
+        self._num_mutants = math.ceil(self._population_size * probability_mutation)
 
         self._pbt_inherit = pbt_inherit
-        self._mu = mu
-        self._evals_done = 0
 
-    def fit(self, X, y=None, logger=None):
+    def _update(self):
+        mutants = self.mutation()
+
+        normal_population = self.population.shape[0] - self._num_elite - self._num_mutants
+        normal_population = max(normal_population, 0)
+        if normal_population > 0:
+            offspring = self.offspring(normal_population)
+            self.population[self._num_elite:, :] = np.vstack((offspring, mutants))
+        else:
+            self.population[self._num_elite:, :] = mutants
+
+        self.calculate_fitness()
+        self._labels = self.decode_solution(self.population[0, :])
+        self.centroids = self.get_centroids(self._labels)
+
+    def _convergence(self):
+        if self._delta is None:
+            logger.debug("Delta is None, convergence cannot be checked.")
+            return False
+        
+        return np.linalg.norm(self._delta) < self.tol
+
+    def _fit(self):
         """
         Ajusta el modelo BRKGA a los datos dados.
 
@@ -73,52 +88,23 @@ class BRKGA(GeneticClustering):
         Returns:
             self: Objeto ajustado.
         """
-        self._data = X
-        self._ml = self.constraints.get_ml_constraints()
-        self._cl = self.constraints.get_cl_constraints()
-        self._dim = X.shape[0]
-
-        self._evals_done = 0
         self.create_population()
-        fitness = self.get_fitness()[0]
-        sorted_fitness = np.argsort(fitness)
-        self._population = self._population[sorted_fitness, :]
-        self._best = cp.deepcopy(self.decode_single_random_key(self._population[0, :]))
-        self._best_fitness = fitness[sorted_fitness[0]]
 
-        offspring_size = self._population_size - (self._num_elite + self._num_mutants)
-        offspring_size = max(offspring_size, 0)
+        iteration = 0
+        while not self.stop_criteria(iteration):
+            self._update()
+            iteration += 1
 
-        for _ in range(self.max_iter):
-            elite = self._population[: self._num_elite, :]
-            non_elite = self._population[self._num_elite :, :]
-
-            mutants = self.mutation()
-
-            if offspring_size > 0:
-                offspring = self.offspring(elite, non_elite, offspring_size)
-                self._population[self._num_elite :, :] = np.vstack((offspring, mutants))
-            else:
-                self._population = np.vstack((elite, mutants))
-
-            self.calculate_fitness()
-            new_best = self._population[self._population_fitness_sorted[0]]
-            new_best_fitness = self._population_fitness[self._population_fitness_sorted[0]]
-            self._population = self._population[sorted_fitness, :]
-
-            if new_best_fitness < self._best_fitness:
-                self._best = cp.deepcopy(self.decode_single_random_key(new_best))
-                self._best_fitness = new_best_fitness
-
-        labels = self.get_labels()
-        self.get_centroids(labels)
         return self
 
     def create_population(self):
         """
         Inicializa la población con valores aleatorios en el rango [0, 1].
         """
-        self._population = np.random.rand(self._population_size, self._dim)
+        self.population = np.random.rand(self._population_size, self._dim)
+
+        self.calculate_fitness()
+
 
     def crossover(self, parent1, parent2):
         """
@@ -132,30 +118,31 @@ class BRKGA(GeneticClustering):
             ndarray: Nuevo cromosoma generado.
         """
         v = np.where(np.random.rand(self._dim) > self._pbt_inherit)[0]
-        new_cromosome = cp.deepcopy(parent1)
+        new_cromosome = parent1
         new_cromosome[v] = parent2[v]
         return new_cromosome
 
-    def offspring(self, elite, non_elite, offspring_size):
+    def offspring(self, offspring_size):
         """
         Genera descendencia cruzando padres elite con no-elite.
 
         Args:
-            elite (ndarray): Subpoblación elitista.
-            non_elite (ndarray): Subpoblación no elitista.
             offspring_size (int): Número de descendientes a generar.
 
         Returns:
             ndarray: Matriz con la descendencia generada.
         """
-        elite_idx = np.random.randint(elite.shape[0], size=offspring_size)
-        non_elite_idx = np.random.randint(non_elite.shape[0], size=offspring_size)
+        elite_idx = np.random.randint(self._num_elite, size=offspring_size)
+        non_elite_idx = np.random.randint(low=self._num_elite, high=self._population_size, size=offspring_size)
         offspring = np.empty((offspring_size, self._dim))
 
-        for i in range(offspring_size):
-            offspring[i, :] = self.crossover(
-                elite[elite_idx[i], :], non_elite[non_elite_idx[i], :]
-            )
+        elites = self.population[elite_idx]
+        non_elites = self.population[non_elite_idx]
+
+        i = 0
+        for elite, non_elite in zip(elites, non_elites):
+            offspring[i, :] = self.crossover(elite, non_elite)
+            i += 1
 
         return offspring
 
---
+++ tests/gac/test_brkga.py
@@ -0,0 +1,93 @@
+import pytest as test
+from clustlib.gac.brkga import BRKGA
+import numpy as np
+from math import sqrt
+
+from mock import patch
+
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+@test.fixture
+def define_brkga():
+    constraints = np.array([
+        [0, 1, -1, -1], 
+        [1, 0, -1, -1], 
+        [-1, -1, 0, 1],
+        [-1, -1, 1, 0]
+    ])
+    brkga = BRKGA(constraints=constraints, n_clusters=2, init="random", max_iter=10)
+    yield brkga
+    del brkga
+
+def test_brkga_initialization(define_brkga):
+    import math
+
+    brkga = define_brkga
+    assert brkga._num_elite == math.ceil(20 * 0.3)
+    assert brkga._num_mutants == math.ceil(20 * 0.2)
+
+def test_brkga_convergence(define_brkga):
+    brkga = define_brkga
+    brkga._delta = 1e-5
+    
+    assert brkga._convergence()
+
+def test_brkga_create_population(define_brkga):
+    brkga = define_brkga
+    X = np.array([[2, 1], [3, 0], [-2, 1], [-3, 0]])
+    brkga.X = X
+    brkga.create_population()
+
+    assert brkga.population.shape[0] == brkga._population_size
+    assert brkga.population.shape[1] == brkga.X.shape[0]
+    assert brkga._population_fitness.shape[0] == brkga._population_size
+
+    best = brkga._population_fitness[0]
+    worst = brkga._population_fitness[-1]
+
+    assert best <= worst, "Best fitness should be less than or equal to worst fitness"
+
+def test_brkga_decode_solution(define_brkga):
+    brkga = define_brkga
+    brkga.n_clusters = 4
+    label = np.array([0, 1, 2, 3])
+    code = np.array([.0, .3, .6, .8])
+    result = brkga.decode_solution(code)
+
+    assert np.all(result == label), "decoded solution does not match expected labels"
+
+def test_brkga_crossover(define_brkga):
+    brkga = define_brkga
+    with patch('numpy.random.rand') as RandMock:
+        mock = np.array([0.1, 0.2, 0.05, 0.7])
+        RandMock.return_value = mock
+        to_cross = np.where(mock < brkga._pbt_inherit)[0]
+
+        parent1 = np.array([0, 1, 2, 3])
+        parent2 = np.array([3, 2, 1, 0])
+        offspring = brkga.crossover(parent1, parent2)
+
+    expected = parent1
+    expected[to_cross] = parent2[to_cross]
+
+    assert np.all(offspring == expected), "Crossover did not produce expected offspring"
+
+def test_brkga_predict(define_brkga):
+    brkga = define_brkga
+    X = np.array([[5, 1], [5, 0], [-5, 0.2], [-5, 0.5]])
+    brkga.max_iter = 100 # Set a high max iteration to ensure convergence
+    brkga.fit(X)
+
+    predictions = brkga.predict(X)
+    assert len(predictions) == len(X)
+    assert predictions[0] == predictions[1], "Predictions do not match labels"
+    assert predictions[2] == predictions[3], "Predictions do not match labels"
